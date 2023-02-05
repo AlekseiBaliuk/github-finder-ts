72a617611be58af387f47cb50769c18a59f528a9
Aleksei Baliuk <aleksred.b@gmail.com>
2023-02-05 20:56:41 +0200

    add-project

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "AlekseiBaliuk",
+  avatar: "https://avatars.githubusercontent.com/u/49524169?v=4",
+  name: "Aleksei Baliuk",
+  company: "",
+  blog: "",
+  location: "Ukraine",
+  bio: "Beginner software developer.",
+  twitter: "",
+  repos: 49,
+  followers: 3,
+  following: 1,
+  created: "2019-04-11T13:11:45Z",
+};
